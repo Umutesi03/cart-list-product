d073962506907850eb5fb3f018313550c0cb0abc
Umutesi03 <keliumutesi@gmail.com>
2025-05-04 17:32:49 +0200

    Add few changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-pink-50 min-h-screen p-6">
       <h1 className="text-3xl font-bold mb-6">Desserts</h1>
-      <div className="flex flex-col md:flex-row gap-6">
+      <div className="flex flex-col lg:flex-row gap-6">
         <div className="flex-1">
           <DessertList />
         </div>
